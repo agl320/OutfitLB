3ffe3e90a16bff85d0e7bc16370545a6fde9178b
Andrew Lai <the.andrew.lai@gmail.com>
2023-02-04 01:15:24 -0800

    Update main-new.py

--- bin/main-new.py
+++ bin/main-new.py
@@ -8,78 +8,6 @@ from PIL import Image, ImageTk
 # removal of prefix
 from ouser import *
 
-class AddFrame(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        # not using super because tkinter uses old way
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
-        
-        # Left type + image frame
-        # Right info frame
-        self.type_frame = tk.Frame(self)
-        self.info_frame = tk.Frame(self)
-
-        # init no image filepath
-        self.filepath_add=""
-
-        self.addType()
-        self.addInfo()
-        self.widgetDisplay()
-
-    def addInfo(self):
-        # Name frame
-        self.add_n_l = tk.Label(self.info_frame, text="Name",justify= tk.LEFT)
-        self.add_n_en = tk.Entry(self.info_frame, width=20)
-
-        # Description frame
-        self.add_d_l = tk.Label(self.info_frame, text="Description",justify= tk.LEFT)
-        self.add_d_txt = tk.Text(self.info_frame, width=15,height=2)
-
-        # Clean? checklist w/ Description frame
-        clean_var =  tk.IntVar()
-        self.add_clean_b = tk.Checkbutton(self.info_frame, text = "Clean?", variable=clean_var)
-
-    def addType(self):
-        self.type_var = tk.IntVar(self.parent, 0) # default value is top
-        self.add_rb_t = tk.Radiobutton(self.type_frame, text="Top", variable=self.type_var, value=0)
-        self.add_rb_b = tk.Radiobutton(self.type_frame, text="Bottom", variable=self.type_var, value=1)
-        self.add_rb_s = tk.Radiobutton(self.type_frame, text="Shoes", variable=self.type_var, value=2)
-
-        self.add_image_b = tk.Button(self.type_frame, text="Image", command=lambda: self.imageUpload())
-
-    def widgetDisplay(self):
-        # INFO FRAME
-        #add_c_en.grid(row=1,column=1,rowspan=1,sticky="N")
-        # name 
-        self.add_n_l.grid(row=0,column=0,sticky="W")
-        self.add_n_en.grid(row=1,column=0,sticky="N")
-
-        # description
-        self.add_d_l.grid(row=2,column=0,sticky="W")
-        self.add_d_txt.grid(row=3,column=0,rowspan=1,sticky="N")
-        self.add_clean_b.grid(row=4,column=0,rowspan=1,sticky="N")
-
-        # TYPE FRAMe
-        self.add_rb_t.grid(row=0,column=0,sticky="W")
-        self.add_rb_b.grid(row=1,column=0,sticky="W")
-        self.add_rb_s.grid(row=2,column=0,sticky="W")
-        # add image (under clothing type)
-        self.add_image_b.grid(row=3,column=0,rowspan=1,sticky="N")
-
-        # MAIN 
-        self.type_frame.grid(row=0, column=0)
-        self.info_frame.grid(row=0, column=1)
-    
-    def imageUpload(self):
-        # clear current filepath
-        self.filepath_add = ""
-        # uploading file (image)
-        self.filepath_add = filedialog.askopenfilename(filetypes = (("jpeg files", "*.jpg"),("png files", "*.png"),("all files","*.*")))
-        
-        # DEBUG
-        print(f">> Updated filepath to: {self.filepath_add}")
-
-
 class PreviewFrame(tk.Frame):
     def __init__(self, parent, all_clothing, clothing_lb, *args, **kwargs):
         # not using super because tkinter uses old way
@@ -151,8 +79,9 @@ class ClosetFrame(tk.Frame):
         self.parent = parent
 
         # <> LEFT LIST AND RIGHT PREVIEW
-        self.lb_frame = tk.Frame(parent)
-        self.lb_preview = tk.Frame(parent)
+        self.lb_frame = tk.Frame(self.parent)
+        self.lb_preview = tk.Frame(self.parent)
+        self.addframe = tk.Frame(self.parent)
 
         # <> CLOSET DISPLAY
         # Scrollbar and Listbox
@@ -168,6 +97,18 @@ class ClosetFrame(tk.Frame):
         self.all_clothing = []
         self.example_data()
 
+        # <> ADD SECTION
+         # Left type + image frame
+        # Right info frame
+        self.type_frame = tk.Frame(self.parent)
+        self.info_frame = tk.Frame(self.parent)
+
+        # init no image filepath
+        self.filepath_add=""
+
+        self.addType()
+        self.addInfo()
+
         # <> WIDGET DISPLAY
         self.widgetDisplay()
 
@@ -178,10 +119,100 @@ class ClosetFrame(tk.Frame):
         """
         self.previewframe = PreviewFrame(self.lb_preview, self.all_clothing, self.clothing_lb) # maybe self.lb_preview?
         self.previewframe.grid(row=0,column=2,sticky="W")
+
+    def addInfo(self):
+        # Name frame
+        self.add_n_l = tk.Label(self.info_frame, text="Name",justify= tk.LEFT)
+        self.add_n_en = tk.Entry(self.info_frame, width=20)
+
+        # Description frame
+        self.add_d_l = tk.Label(self.info_frame, text="Description",justify= tk.LEFT)
+        self.add_d_txt = tk.Text(self.info_frame, width=15,height=2)
+
+        # Clean? checklist w/ Description frame
+        self.clean_var =  tk.IntVar()
+        self.add_clean_b = tk.Checkbutton(self.info_frame, text = "Clean?", variable=self.clean_var)
+
+        # Button submit
+        self.add_sm_b = tk.Button(
+            self.info_frame,
+            text="ADD", 
+            padx=10, 
+            pady=5,
+            command=lambda: self.addClothing()
+            )
+
+
+    def addType(self):
+        self.type_var = tk.IntVar(self.parent, 0) # default value is top
+        self.add_rb_t = tk.Radiobutton(self.type_frame, text="Top", variable=self.type_var, value=0)
+        self.add_rb_b = tk.Radiobutton(self.type_frame, text="Bottom", variable=self.type_var, value=1)
+        self.add_rb_s = tk.Radiobutton(self.type_frame, text="Shoes", variable=self.type_var, value=2)
+
+        self.add_image_b = tk.Button(self.type_frame, text="Image", command=lambda: self.imageUpload())
+
+    def imageUpload(self):
+        # clear current filepath
+        self.filepath_add = ""
+        # uploading file (image)
+        self.filepath_add = filedialog.askopenfilename(filetypes = (("jpeg files", "*.jpg"),("png files", "*.png"),("all files","*.*")))
+        
+        # DEBUG
+        print(f">> Updated filepath to: {self.filepath_add}")
+
+    # Add clothing to clothing list function
+    def addClothing(self):
+
+        # get name and description 
+        self.add_n_en = self.add_n_en.get()
+        self.add_d_txt = self.add_d_txt.get("1.0",tk.END).replace('\n',' ')
+        #new_c_name = str(input())
+
+        # If empty string, use no name
+        if not self.add_n_en or self.add_n_en.isspace():
+            self.add_n_en = "No name"
+
+        # debug print
+        print(f"[+ type:{self.type_var.get()}] {self.add_n_en}")
+
+        # Adding to clothing list -> type check
+        match self.type_var.get():
+            case 0:
+                self.all_clothing.append(Top(self.add_n_en, self.add_d_txt, "Custom", clean=self.clean_var.get(), filepath=self.filepath_add))
+            case 1:
+                self.all_clothing.append(Bottom(self.add_n_en, self.add_d_txt, "Custom", clean=self.clean_var.get(), filepath=self.filepath_add))
+            case 2:
+                self.all_clothing.append(Shoes(self.add_n_en, self.add_d_txt, "Custom", clean=self.clean_var.get(), filepath=self.filepath_add))
+        
+        # RESET filepath
+        self.filepath_add=""
+        
+
+        # Check if clean
+        if self.clean_var.get():#[].is_clean():
+            self.clothing_lb.insert(tk.END, self.add_n_en)
+            self.clothing_lb.itemconfig(tk.END,{'bg':'Green'})
+        else:
+            self.clothing_lb.insert(tk.END, self.add_n_en)
+            self.clothing_lb.itemconfig(tk.END,{'bg':'Red'})
+        
     
     # WIDGET DISPLAYING
     # GRID FORMAT
     def widgetDisplay(self):
+        """
+        MAIN FRAME 
+        """
+        self.lb_frame.grid(row=0,column=0,columnspan=3)
+        self.type_frame.grid(row=1, column=0)
+        self.info_frame.grid(row=1, column=1)
+        #addClothing_en.grid(row=1,column=1,rowspan=1,sticky="N")
+        # name 
+        self.addframe.grid(row=1,column=0,rowspan=3,sticky="NW")
+        self.lb_preview.grid(row=0,column=3)
+        """
+        LISTBOX FRAME
+        """
         # lb_frame
         self.clothing_lb.grid(row=0,column=0,columnspan=3)
         self.sb.grid(row=0,column=2, sticky='NSE')
@@ -189,9 +220,31 @@ class ClosetFrame(tk.Frame):
         # lb_preview
         self.prev_b.grid(row=0,column=1,sticky="W")
 
-        # MAIN FRAME DISPLAY
-        self.lb_frame.grid(row=0,column=0)
-        self.lb_preview.grid(row=0,column=1)
+        """
+        INFO ADD FRAME 
+        """
+        
+
+        self.add_n_l.grid(row=0,column=0,sticky="W")
+        self.add_n_en.grid(row=1,column=0,sticky="N")
+
+        # description
+        self.add_d_l.grid(row=2,column=0,sticky="W")
+        self.add_d_txt.grid(row=3,column=0,rowspan=1,sticky="N")
+        self.add_clean_b.grid(row=4,column=0,rowspan=1,sticky="N")
+
+         # button submit
+        self.add_sm_b.grid(row=5,column=0, sticky="S")
+        
+        """
+        TYPE FRAME 
+        """
+        self.add_rb_t.grid(row=0,column=0,sticky="W")
+        self.add_rb_b.grid(row=1,column=0,sticky="W")
+        self.add_rb_s.grid(row=2,column=0,sticky="W")
+        # add image (under clothing type)
+        self.add_image_b.grid(row=3,column=0,rowspan=1,sticky="N")
+        
         
 
     def example_data(self):
@@ -222,8 +275,6 @@ class MainApplication(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         self.cframe = ClosetFrame(self)
-        self.addframe = AddFrame(self)
-        
 
         parent.geometry('650x500')
         parent.title("Outfit Manager")
@@ -231,7 +282,6 @@ class MainApplication(tk.Frame):
         # widget placement
         self.grid(padx=5, pady=5)
         self.cframe.grid(row=0,column=0,columnspan=3,sticky="W")
-        self.addframe.grid(row=1,column=0,rowspan=3,sticky="W")
         
         
 
